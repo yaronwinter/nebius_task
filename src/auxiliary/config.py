6f6d0416da634e7a50d07bcdaea61358078ffa48
yaronwinter <yaron.winter@gmail.com>
2026-02-25 17:04:54 +0200

    nebius task

--- src/auxiliary/config.py
+++ src/auxiliary/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 LLM_PROVIDER = os.getenv("LLM_PROVIDER", "openai")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-CONSIDER_SRC_FILES = os.getenv("CONSIDER_SRC_FILES")
+CONSIDER_SRC_FILES = os.getenv("CONSIDER_SRC_FILES", 0)
 DEFAULT_LLM_MODEL = "gpt-4o-mini"
 
 PROMPTS_FOLDER = "src/prompts/"
